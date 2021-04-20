6dd23abd45f59af26e9e63e73c4377ba1e3c75e6
lakshaymadhav <lakshaymadhav25@gmail.com>
2021-04-20 10:53:31 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-ppwqyp
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-ppwqyp)
---
+++ src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component, VERSION } from "@angular/core";
+
+@Component({
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
+  template: `
+    <button (click)="onClickMe()">Click me!</button>
+    {{ clickMessage }}
+  `
+})
+export class AppComponent {
+  name = "Angular " + VERSION.major;
+  clickMessage = "";
+
+  work() {
+    this.clickMessage = "You are my hero!";
+  }
+}
